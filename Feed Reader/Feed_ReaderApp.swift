1affdfb0266156df7d54496cc833823b0a6b3d24
LogicAndLines <betulzeynepseyis@gmail.com>
2025-07-31 17:42:16 +0200

    Initial Commit

---
+++ Feed Reader/Feed_ReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Feed_ReaderApp.swift
+//  Feed Reader
+//
+//  Created by Zeynep Turnalı on 31.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct Feed_ReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Feed ReaderUITests/Feed_ReaderUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Feed_ReaderUITestsLaunchTests.swift
+//  Feed ReaderUITests
+//
+//  Created by Zeynep Turnalı on 31.07.2025.
+//
+
+import XCTest
+
+final class Feed_ReaderUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
